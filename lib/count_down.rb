9c06e51076c09c53d993e2cd20e26c0721e987d6
Natalia Galan <n8galan@gmail.com>
2020-06-26 21:56:38 +0000

    Automatically backed up by Learn

--- lib/count_down.rb
+++ lib/count_down.rb
@@ -1,7 +1,13 @@
 # Write your code here
-count = 10
-while count >= 1 do
- puts count
-count -= 1
+# count = 10
+# while count >= 1 do
+#  puts count
+# count -= 1
+# end
+# puts "Happy New Year!"
+
+counter = 0
+until counter == 20
+  puts "The current number is less than 20."
+  counter += 1
 end
-puts "Happy New Year!"
