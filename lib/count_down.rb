bb02527637234d70fbb1d4588b94b247cc61620a
Natalia Galan <n8galan@gmail.com>
2020-06-26 21:59:07 +0000

    Automatically backed up by Learn

--- lib/count_down.rb
+++ lib/count_down.rb
@@ -1,7 +1,14 @@
 # Write your code here
-count = 10
-while count >= 1 do
- puts count
-count -= 1
+# count = 10
+# while count >= 1 do
+#  puts count
+# count -= 1
+# end
+# puts "Happy New Year!"
+
+
+count = 0
+while count < 3 do
+  puts "#{count}"
+  count = count + 1
 end
-puts "Happy New Year!"
